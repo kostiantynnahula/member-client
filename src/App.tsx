6d479d61abf2c7f1cd8c684e98618453d26a4ae0
Kostia Nahula <kostyannagula@gmail.com>
2022-11-20 10:16:30 +0200

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import FacebookLogin from 'react-facebook-login';
+import { GoogleLogin } from 'react-google-login';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +20,22 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <FacebookLogin
+        appId='1786612898371193'
+        autoLoad={false}
+        fields={'name,email,picture'}
+        scope={'public_profile,email'}
+        callback={(res) => console.log(res)}
+      />
+      <GoogleLogin
+        clientId='677821976500-b7mf7piiu9hp8n4bm7935g92hpi5g6b3.apps.googleusercontent.com'
+        buttonText='Google login'
+        onSuccess={(res) => console.log(res)}
+        onFailure={(err) => console.log(err)}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+      />
     </div>
   );
 }
