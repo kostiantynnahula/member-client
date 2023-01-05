198dc9f1bea2e532233001dd6d4166f863ca1fb5
Kostia Nahula <kostyannagula@gmail.com>
2023-01-05 07:25:17 +0200

    created new components

---
+++ src/components/Pages/Folder/Folder.tsx
@@ -0,0 +1,7 @@
+export const Folder = () => {
+  return (
+    <>
+    Folder component
+    </>
+  );   
+}
--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -1,11 +1,21 @@
-import { Dashboard } from '../components/Pages/Dashboard/Dashboard';
 import { createBrowserRouter } from 'react-router-dom';
 import { Error } from '../components/Layout/Error/Error';
+import { Dashboard } from '../components/Pages/Dashboard/Dashboard';
+import { Folder } from './../components/Pages/Folder/Folder';
+import { Profile } from './../components/Pages/Profile/Profile';
 
 export const privateRoutes = createBrowserRouter([
   {
     path: '/',
     element: <Dashboard/>,
     errorElement: <Error/> 
+  }, 
+  {
+    path: '/folder/:id',
+    element: <Folder/>
+  }, 
+  {
+    path: 'profile',
+    element: <Profile/>
   } 
 ]);
