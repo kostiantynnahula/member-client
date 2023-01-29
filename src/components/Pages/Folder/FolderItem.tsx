d88d58fee2c77e91b868e44fb7828d8baab60edc
Kostia Nahula <kostyannagula@gmail.com>
2023-01-29 21:40:03 +0200

    Update folder item component

--- src/components/Pages/Folder/FolderItem.tsx
+++ src/components/Pages/Folder/FolderItem.tsx
@@ -1,16 +1,43 @@
+import { useState } from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { BsFolderFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import { IFolderItemProp } from './interfaces';
+import './FolderItem.scss';
 
 export const FolderItem = (prop: IFolderItemProp) => {
+
+  const title = (<><BsFolderFill/> {prop.name}</>);
+
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const onOpenContextMenu = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    setMenuOpen(true);
+  }
+
+  const onEditFolder = () => {
+    console.log('on edit folder');
+  }
+
+  const onDeleteFolder = () => {
+    console.log('on delete folder');
+  }
+  
   return (
-    <>
-      <Link
+    <div className='folder-item'>
+      <DropdownButton 
+        as={Link} 
+        title={title}
+        show={menuOpen}
         to={`folder/${prop.id}`}
-        className='btn btn-outline-primary w-100 text-left'
+        onToggle={() => setMenuOpen(false)}
+        onContextMenu={onOpenContextMenu}
+        variant={'outline-primary'}
       >
-        <BsFolderFill/> {prop.name}
-      </Link>
-    </>
+        <Dropdown.Item onClick={onEditFolder}>Edit</Dropdown.Item>
+        <Dropdown.Item onClick={onDeleteFolder}>Delete</Dropdown.Item>
+      </DropdownButton>
+    </div>
   );   
 }
